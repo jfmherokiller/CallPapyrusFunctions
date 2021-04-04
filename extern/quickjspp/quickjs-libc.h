80de919cdac23ded8c0eff935357e6c635943a0c
noah the goodra <peterpan0413@live.com>
2021-04-03 21:02:26 -0500

    export the new functions

--- extern/quickjspp/quickjs-libc.h
+++ extern/quickjspp/quickjs-libc.h
@@ -41,6 +41,8 @@ void js_std_init_handlers(JSRuntime *rt);
 void js_std_free_handlers(JSRuntime *rt);
 void js_std_dump_error(JSContext *ctx);
 uint8_t *js_load_file(JSContext *ctx, size_t *pbuf_len, const char *filename);
+char* js_std_dump_error_buffer(JSContext* ctx);
+void js_std_loop_buffer(JSContext* ctx, void (*errorHandler)(char*));
 int js_module_set_import_meta(JSContext *ctx, JSValueConst func_val,
                               JS_BOOL use_realpath, JS_BOOL is_main);
 JSModuleDef *js_module_loader(JSContext *ctx,
