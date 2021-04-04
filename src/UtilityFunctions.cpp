2b11c360b563ef6a24c27997fae9b2aad023d0e7
noah the goodra <peterpan0413@live.com>
2021-04-03 21:04:04 -0500

    add console free function

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -1,15 +1,25 @@
-﻿#include "quickjs.h"
+﻿
 
 extern void PrintToConsole(JSContext* ctx,JSValue val)
 {
 	const auto* const stats = JS_ToCString(ctx, val);
 	if (std::strcmp(stats, "undefined") == 0)
+	{
+		JS_FreeCString(ctx, stats);
 		return;
+	}
 	RE::ConsoleLog::GetSingleton()->Print("jseval:%s",stats);
-	logger::info(FMT_STRING("jseval {}"), stats);
+	logger::info(FMT_STRING("jseval:{}"), stats);
+	JS_FreeCString(ctx, stats);
 }
-extern void PrintStringToConsoleFile(std::string Part)
+extern void PrintStringToConsoleFile(const char* Part)
 {
-	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part.c_str());
-	logger::info(FMT_STRING("jseval {}"), Part.c_str());
+	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
+	logger::info(FMT_STRING("jseval:{}"), Part);
+}
+extern void PrintStringToConsoleFileFree(char* Part)
+{
+	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
+	logger::info(FMT_STRING("jseval:{}"), Part);
+	free(Part);
 }
