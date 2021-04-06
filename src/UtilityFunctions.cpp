73493e6e5eccd04702250327f01518bc157c1766
noah the goodra <peterpan0413@live.com>
2021-04-05 20:34:37 -0500

    add output function

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -17,6 +17,11 @@ extern void PrintStringToConsoleFile(const char* Part)
 	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
 	logger::info(FMT_STRING("jseval:{}"), Part);
 }
+extern void PrintStringToConsoleFile2(const std::string& str)
+{
+	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", str.c_str());
+	logger::info(FMT_STRING("jseval:{}"), str.c_str());
+}
 extern void PrintStringToConsoleFileFree(char* Part)
 {
 	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
