7f8ec1a73b5217236b9aa68b37f7f1c3d88c1bab
noah the goodra <peterpan0413@live.com>
2021-04-26 16:23:18 -0500

    remove the unused functions

--- src/UtilityFunctions.cpp
+++ src/UtilityFunctions.cpp
@@ -1,5 +1,4 @@
-﻿
-std::vector<std::string> Splitter(const std::string& basetext, const char delim)
+﻿std::vector<std::string> Splitter(const std::string& basetext, const char delim)
 {
 	std::string buf;				 // Have a buffer string
 	std::stringstream ss(basetext);	 // Insert the string into a stream
@@ -9,20 +8,4 @@ std::vector<std::string> Splitter(const std::string& basetext, const char delim)
 	while (getline(ss, buf, delim))
 		tokens.push_back(buf);
 	return tokens;
-}
-extern void PrintStringToConsoleFile(const char* Part)
-{
-	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
-	logger::info(FMT_STRING("jseval:{}"), Part);
-}
-extern void PrintStringToConsoleFile2(const std::string& str)
-{
-	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", str.c_str());
-	logger::info(FMT_STRING("jseval:{}"), str.c_str());
-}
-extern void PrintStringToConsoleFileFree(char* Part)
-{
-	RE::ConsoleLog::GetSingleton()->Print("jseval:%s", Part);
-	logger::info(FMT_STRING("jseval:{}"), Part);
-	free(Part);
 }
